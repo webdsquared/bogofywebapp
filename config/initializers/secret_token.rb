02c03df690333ca00f0a12b66db09131f9e079e1
Aaron Johnson <aaronjohnsonis@gmail.com>
2013-06-07 07:58:01 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bogofywebapp::Application.config.secret_token = 'a555de720d21796b5253ac18c91637be3d19577e610024cdfe0a310bd74fad1d6624e6721a1c65428ac68416ff5de1c891b7a8ca706f4cbbdc31d52759bf0754'
